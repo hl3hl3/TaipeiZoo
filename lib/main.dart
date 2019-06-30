91e5a05ad543bd3a0da3c1cf63f58d646f8b46da
Rebecca Hsieh <rebecca.hsieh77@gmail.com>
2019-06-30 18:09:08 +0800

    調整 場館列表項目 文字區塊撐開
    // TODO 依照需求調整 item ui

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,14 @@ class StoreListPage extends StatelessWidget {
                     "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg/440px-An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg",
                     width: 120,
                 ),
-                Column(
-                  children: <Widget>[
-                    Text('標題'),
-                    Text('說明'),
-                    Text('時間'),
-                  ],
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      Text('標題'),
+                      Text('說明'),
+                      Text('時間'),
+                    ],
+                  ),
                 ),
                 Icon(Icons.arrow_forward_ios),
               ],
