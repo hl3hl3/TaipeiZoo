82d74e0ac13d972115c3fe4f041686e40b23cf4d
Rebecca Hsieh <rebecca.hsieh77@gmail.com>
2019-06-30 18:13:10 +0800

    調整 場館列表項目 元件間距
    add padding & sizedbox
    
    // TODO 依照需求調整 item ui

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,30 @@ class StoreListPage extends StatelessWidget {
           itemBuilder: (BuildContext context, int index) {
             return Row(
               children: <Widget>[
-                Image.network(
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  child: Image.network(
                     "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg/440px-An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg",
                     width: 120,
+                  ),
                 ),
                 Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Text('標題'),
+                      SizedBox(height: 8),
                       Text('說明'),
+                      SizedBox(height: 8),
                       Text('時間'),
                     ],
                   ),
                 ),
-                Icon(Icons.arrow_forward_ios),
+                Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: Icon(Icons.arrow_forward_ios),
+                ),
               ],
             );
           },
