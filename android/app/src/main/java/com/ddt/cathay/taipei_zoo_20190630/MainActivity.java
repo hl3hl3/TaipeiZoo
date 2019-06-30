d226b57eb75c8daf599f996708d25f1e98398cdb
Rebecca Hsieh <rebecca.hsieh77@gmail.com>
2019-06-30 17:50:01 +0800

    init commit

---
+++ android/app/src/main/java/com/ddt/cathay/taipei_zoo_20190630/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ddt.cathay.taipei_zoo_20190630;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
